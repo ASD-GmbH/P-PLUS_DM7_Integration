e59ea237ec97ee8a3afde07e71b5fdf1eba252d3
Philip Jander <pjander@a-s-d.de>
2018-03-05 12:29:01 +0100

    Demo Projekt umbenannt

--- Demo_Implementierung/Properties/AssemblyInfo.cs
+++ Demo_Implementierung/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ramjam")]
+[assembly: AssemblyTitle("Demo_Implementierung")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ramjam")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Demo_Implementierung")]
+[assembly: AssemblyCopyright("Copyright ©  2017 - ASD Personalinformatinssysteme")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
